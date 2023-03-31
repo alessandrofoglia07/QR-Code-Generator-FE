b05276c8f97741d971b858ccec5fa7bdcc60ff7d
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-03-31 18:49:15 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# QR Code Generator
+Frontend of QR Code generator with React
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Paper, TextField, Button, Stack, Typography } from '@mui/material';
+import Axios from 'axios';
 
-function App() {
+export const App = () => {
+
+  const [input, setInput] = useState<string>('');
+  const [qrCode, setQrCode] = useState<string>('');
+
+  const generateQrCode = async () => {
+    if (input === '' || input == null) {
+      setQrCode('');
+    } else {
+      console.log('Generating with input ' + input);
+      try {
+        const res = await Axios.post('http://localhost:3002/qrcode', { input });
+        setQrCode(res.data);
+        console.log(res.data);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Paper elevation={3} className="paper">
+        <Stack spacing={2} direction="column" className="paperZone">
+          <Typography variant="h3" className="poppinsFont title" sx={{ fontSize: '2.5em', fontFamily: 'Poppins, sans-serif', fontWeight: '600', marginTop: '1em'}}>
+            QR Code Generator
+          </Typography>
+          <TextField
+          label="Enter your text / url"
+          variant="outlined"
+          className="poppinsFont input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          sx={{ width: '80%', alignSelf: 'center', top: '5vh', position: 'relative'}}
+          />
+          <Button 
+          variant="contained" 
+          className="button"
+          sx={{ width: '80%', alignSelf: 'center', top: '5vh', position: 'relative', color: 'white'}}
+          onClick={generateQrCode}>
+            Generate QR Code
+          </Button>
+          { qrCode ? <img src={qrCode} alt="QR Code" className="qrCode"/> : null}
+        </Stack>
+      </Paper>
     </div>
   );
-}
-
-export default App;
+};
